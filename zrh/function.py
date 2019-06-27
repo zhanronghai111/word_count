a0ac167f6351973d14a3c7ba2107f7a822326691
zhanronghai <2364342507@qq.com>
2019-06-27 22:12:26 +0800

    添加了源文件

---
+++ zrh/function.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, '1.html')
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(user_text)
+    dict_word = {}
+
+    for word in user_text:
+        if word not in dict_word:
+            dict_word[word] = 1
+        else:
+            dict_word[word] += 1
+
+    return render(request,'count.html',{'count':total_count,'text':user_text,
+                                        'dicword':dict_word})
+
